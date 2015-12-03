8b90e87ddb9eed10aa0883b056f0403d7dd3501d
ryanriddle@berkeley.edu <ryanriddle@berkeley.edu>
2015-12-02 19:20:01 -0800

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Make Admin
+
+#Make TAs
+%w(Howard Charles Sam Junyu Erica Warren Mei).each do |name|
+  Teaching_assistants.create! name: name, email: name+"@gmail.com", password: 'password12345', password_confirmation: 'password12345', course: 'rails'
+end
+
+i = 0
+%w(Anthony Caydia Hubert Ryan).each do |name|
+  Admins.create! name: name, email: name+"@gmail.com", password: 'password12345', password_confirmation: 'password12345', section_id: i
+  i = i + 1
+end
+
+%w(stu1 stu2 stu3 stu4 stu5 stu6 stu7 stu8 stu9 stu10).each do |name|
+  Students.create! name: name, email: name+"@gmail.com", password: 'password12345', password_confirmation: 'password12345' 
+end
